83a01080f4e92c462c76234a930cb9e273aaef65
Lesley <lesleylai@lesleylai.info>
2019-11-05 20:50:33 -0700

    Add particles

--- src/Main.elm
+++ src/Main.elm
@@ -6,13 +6,13 @@ import Canvas exposing (..)
 import Canvas.Settings exposing (..)
 import Canvas.Settings.Advanced exposing (..)
 import Color
-import Html exposing (Html, div)
+import Html exposing (Html, button, h3, div, input, span)
 import Html.Attributes exposing (style)
+import Html.Events exposing (onClick, onInput)
 
 import Array exposing (Array)
 
 import Vector2 exposing (Vec2)
-
 
 type alias Particle =
     { position: Vec2
@@ -20,21 +20,29 @@ type alias Particle =
     }
 
 type alias Model =
-    { particles : Array Particle }
-
+    { particles : Array Particle,
+      particleToAdd : Particle }
 
 type Msg
     = Frame Float
+    | ChangeParticleToAddPosX String
+    | ChangeParticleToAddPosY String
+    | ChangeParticleToAddVelX String
+    | ChangeParticleToAddVelY String
+    | AddParticle
+
+idleParticle : Particle
+idleParticle =  { position = { x = 0, y = 0 }
+                , velocity = { x = 0, y = 0 }
+                }
 
 initParticles : Array Particle
-initParticles = Array.initialize 1 (\x -> { position = { x = 0, y = 0 }
-                                          , velocity = { x = 0, y = 1 }
-                                          })
+initParticles = Array.empty
 
 main : Program () Model Msg
 main =
     Browser.element
-        { init = \_ -> ({ particles = initParticles }, Cmd.none )
+        { init = \_ -> ({ particles = initParticles, particleToAdd = idleParticle }, Cmd.none )
         , view = view
         , update = update
         , subscriptions = \model -> onAnimationFrameDelta Frame
@@ -42,8 +50,8 @@ main =
 
 updateParticle : Particle -> Particle
 updateParticle particle =
-    { particle
-        | position = Vector2.add particle.position particle.velocity }
+    let newVel = Vector2.add particle.velocity (Vec2 0 0.98) in
+    { position = Vector2.add particle.position particle.velocity, velocity = newVel }
 
 update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
@@ -51,13 +59,69 @@ update msg model =
         Frame _ ->
             let newParticles = Array.map updateParticle model.particles
             in
-            ( { particles = newParticles }, Cmd.none )
+            ( { model | particles = newParticles }, Cmd.none )
+        ChangeParticleToAddPosX x ->
+            let oldParticleToAdd = model.particleToAdd in
+            let oldPos = oldParticleToAdd.position in
+            let newPos = { oldPos | x = Maybe.withDefault 0.0 (String.toFloat x) } in
+            let newParticleToAdd = { oldParticleToAdd | position = newPos } in
+            ( { model | particleToAdd = newParticleToAdd }, Cmd.none )
+        ChangeParticleToAddPosY y ->
+            let oldParticleToAdd = model.particleToAdd in
+            let oldPos = oldParticleToAdd.position in
+            let newPos = { oldPos | y = Maybe.withDefault 0.0 (String.toFloat y) } in
+            let newParticleToAdd = { oldParticleToAdd | position = newPos } in
+            ( { model | particleToAdd = newParticleToAdd }, Cmd.none )
+        ChangeParticleToAddVelX x ->
+            let oldParticleToAdd = model.particleToAdd in
+            let oldVel = oldParticleToAdd.velocity in
+            let newVel = { oldVel | x = Maybe.withDefault 0.0 (String.toFloat x) } in
+            let newParticleToAdd = { oldParticleToAdd | velocity = newVel } in
+            ( { model | particleToAdd = newParticleToAdd }, Cmd.none )
+        ChangeParticleToAddVelY y ->
+            let oldParticleToAdd = model.particleToAdd in
+            let oldVel = oldParticleToAdd.velocity in
+            let newVel = { oldVel | y = Maybe.withDefault 0.0 (String.toFloat y) } in
+            let newParticleToAdd = { oldParticleToAdd | velocity = newVel } in
+            ( { model | particleToAdd = newParticleToAdd }, Cmd.none )
+        AddParticle ->
+            ( { model | particles = (Array.push model.particleToAdd model.particles) }, Cmd.none )
 
 width = 400
 height = 400
 centerX = width / 2
 centerY = height / 2
 
+
+numberInput : String -> String -> (String -> Msg) -> Html Msg
+numberInput name placeholder onInputMsg
+    = input [ Html.Attributes.type_ "number"
+            , Html.Attributes.name name
+            , Html.Attributes.value "0"
+            , Html.Attributes.placeholder placeholder
+            , Html.Attributes.required True
+            , Html.Attributes.step "0.01"
+            , onInput onInputMsg ] []
+
+inputsView : Html Msg
+inputsView =
+    div
+        [ style "display" "flex"
+        , style "flex-direction" "column"
+        ]
+        [ h3 [] [ Html.text "Add New Particle" ]
+        , div []
+            [ span [][Html.text "position" ]
+            , numberInput "positionX" "x" ChangeParticleToAddPosX
+            , numberInput "positionY" "y" ChangeParticleToAddPosY
+            ]
+        , div []
+            [ span [][ Html.text "velocity" ]
+            , numberInput "velocityX" "x" ChangeParticleToAddVelX
+            , numberInput "velocityY" "y" ChangeParticleToAddVelY
+            ]
+        , button [ onClick AddParticle ] [ Html.text "Add" ]
+        ]
 
 view : Model -> Html Msg
 view { particles } =
@@ -72,6 +136,7 @@ view { particles } =
             [ clearScreen
             , render particles
             ]
+        , inputsView
         ]
 
 
